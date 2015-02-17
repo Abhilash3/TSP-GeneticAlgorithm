ba41c866199ea649c6999c266e33e10992634208
Abhilash Kumar Vedwan <abhi211193@gmail.com>
2015-02-17 05:53:17 -0500

    Update CoordinateGenerator.java
    making sure that a new group of coordinates is created every single time

--- src/coordinate/CoordinateGenerator.java
+++ src/coordinate/CoordinateGenerator.java
@@ -9,16 +9,14 @@ import java.util.Random;
 public class CoordinateGenerator {
 
 	public static void main(String[] args) {
-		PrintWriter out;
 		try {
-			out = new PrintWriter(new BufferedWriter(new FileWriter("coordinates.txt", true)));
+			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("coordinates.txt", false)));
         	for (int i = 0; i < 5000; i++) {
         		out.print((new Random().nextInt(115) + 5) * 10 + " ");
         		out.print((new Random().nextInt(55) + 5) * 10 + "\n");
             }
 		    out.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
